49e30bcbdb9f7e33a7167c4cd6c54e9c81fd82a7
Michael König <36195136+koenigscode@users.noreply.github.com>
2019-03-18 20:42:02 +0100

    add string formatting

---
+++ content/partials/basic_syntax/comments_multiline_strings.py
@@ -0,0 +1,13 @@
+# this is a single-line comment
+
+"""
+This is a
+docstring/multi-line comment
+"""
+
+x = """
+You can create a
+multiline string using triple "
+"""
+
+print(x)
---
+++ content/partials/basic_syntax/string_formatting_examples.py
@@ -0,0 +1,7 @@
+who = "I"
+version = 3
+
+print( who + " love Python " + str(version) )
+print( "%s love Python %s" % (who, version) )
+print( "{} love Python {}".format(who, version) )
+print( f"{who} love Python {version}" )
