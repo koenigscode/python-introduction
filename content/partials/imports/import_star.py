fd8afa5cab78d1b4a16ad46ce36984cd62441210
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-20 15:55:58 +0100

    add import section

---
+++ content/partials/imports/import_star.py
@@ -0,0 +1,12 @@
+# a.py:
+# some_var = 1
+
+# b.py:
+# some_var = 0
+
+from module_a import *  # better: import module_a as a
+from module_b import *  # better: import module_b as b
+
+print(some_var)
+# better: print(a.some_var)
+# better: print(b.some_var)
