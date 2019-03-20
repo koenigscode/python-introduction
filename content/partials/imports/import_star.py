e175ed84d680e2b52e2f5bf2a3452a20937f4866
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-20 19:44:09 +0100

    small fixes and improvements

--- content/partials/imports/import_star.py
+++ content/partials/imports/import_star.py
@@ -1,11 +1,14 @@
-# a.py:
+"""content of module_a.py:
 # some_var = 1
+"""
 
-# b.py:
+"""content of module_b.py:
 # some_var = 0
+"""
 
 from module_a import *  # better: import module_a as a
 from module_b import *  # better: import module_b as b
+
 
 print(some_var)
 # better: print(a.some_var)
