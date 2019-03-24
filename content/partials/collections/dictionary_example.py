8baa861b8ae5fb46db2443e532e94c046ccfc240
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-24 15:31:24 +0100

    add dictionary section

---
+++ content/partials/collections/dictionary_example.py
@@ -0,0 +1,2 @@
+iterable = [[1, "one"], [2, "two"], [3, "three"]]
+print(dict(iterable))
---
+++ content/partials/collections/iterating_dictionaries.py
@@ -0,0 +1,5 @@
+d = {1: "one", 2: "two", 3: "three"}
+print(d.items())
+
+for key, val in d.items():
+    print(f"{key}: {val}")
