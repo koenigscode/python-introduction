e7edae65f1249bcd97d76eb8d96ca6153bf96373
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-24 11:02:06 +0100

    make new generators example

--- content/partials/generators/gen.py
+++ content/partials/generators/gen.py
@@ -1,9 +1,20 @@
-def gen():
-  for x in range(10**10000):
-    yield x**2
+def primes_generator():
+    yield 2  # yield two
 
-g = gen()
-print(g)
-print(next(g))
-print(next(g))
-print(next(g))
+    number = 3  # then start with 3
+    while True:
+        for div in range(2, number):
+            if number % div == 0:
+                number += 1
+            else:
+                yield number  # yield the prime number
+                number += 1
+
+
+# get an instance of the generator
+primes = primes_generator()
+print(f"primes object: {primes}")
+
+# print first 10 primes
+for i in range(10):
+    print(next(primes))
