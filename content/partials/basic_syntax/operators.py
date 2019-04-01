d0fcec0b5ece23f6fb0b421f0a4d35347ee058cb
koenigscode <koenigmichael2002@gmail.com>
2019-04-01 19:38:03 +0200

    add operators, read from command line and parse numbers section

---
+++ content/partials/basic_syntax/operators.py
@@ -0,0 +1,7 @@
+x = 0
+print( 3 ** 2 ) # 3 to the power of 2
+print( 3 // 2 ) # integer division (floor of normal division)
+
+x += 1 # increment x by one
+# of couse also possible with -=, +=, /*, %=
+print(x)
---
+++ content/partials/basic_syntax/parse_numbers.py
@@ -0,0 +1,5 @@
+print(int("3"))
+print(float("3.14"))
+
+# Raises an error:
+int("hello")
